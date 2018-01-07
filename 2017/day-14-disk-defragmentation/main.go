77eac5048d881ba3cf8c5c308555104d429861e8
Milo Lee <leexu.job@gmail.com>
2018-01-07 12:11:58 +0800

    2017: finish day 14 - part two

--- 2017/day-14-disk-defragmentation/main.go
+++ 2017/day-14-disk-defragmentation/main.go
@@ -9,9 +9,8 @@ import (
 )
 
 func main() {
-	input := `uugsqrei`
-
-	fmt.Println(partOne(input))
+	fmt.Println(partOne("uugsqrei"))
+	fmt.Println(partTwo("uugsqrei"))
 }
 
 func partOne(input string) int {
@@ -27,4 +26,91 @@ func partOne(input string) int {
 	}
 
 	return count
+}
+
+func partTwo(input string) int {
+	hashes := make([]string, 128)
+
+	for i := 0; i < 128; i++ {
+		binary, hash := "", knothash.Hash(input+"-"+strconv.Itoa(i))
+
+		for j := 0; j < 4; j++ {
+			v, _ := strconv.ParseInt(hash[j*8:(j+1)*8], 16, 64)
+			s := strconv.FormatInt(v, 2)
+
+			for {
+				if len(s) == 32 {
+					break
+				}
+				s = "0" + s
+			}
+			binary += s
+		}
+
+		hashes[i] = binary
+	}
+
+	count, stat := 0, map[string]bool{}
+
+	for r, hash := range hashes {
+		for c, char := range hash {
+			i, j := strconv.Itoa(r), strconv.Itoa(c)
+			key := i + "-" + j
+
+			if _, isPresent := stat[key]; isPresent {
+				continue
+			}
+
+			if string(char) == "1" {
+				stat[key] = true
+				walkRegion(r, c, hashes, stat)
+
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func walkRegion(r int, c int, hashes []string, stat map[string]bool) {
+	if top := r - 1; top >= 0 && string(hashes[top][c]) == "1" {
+		i, j := strconv.Itoa(top), strconv.Itoa(c)
+		key := i + "-" + j
+
+		if _, isPresent := stat[key]; !isPresent {
+			stat[key] = true
+			walkRegion(top, c, hashes, stat)
+		}
+	}
+
+	if right := c + 1; right < 128 && string(hashes[r][right]) == "1" {
+		i, j := strconv.Itoa(r), strconv.Itoa(right)
+		key := i + "-" + j
+
+		if _, isPresent := stat[key]; !isPresent {
+			stat[key] = true
+			walkRegion(r, right, hashes, stat)
+		}
+	}
+
+	if bottom := r + 1; bottom < 128 && string(hashes[bottom][c]) == "1" {
+		i, j := strconv.Itoa(bottom), strconv.Itoa(c)
+		key := i + "-" + j
+
+		if _, isPresent := stat[key]; !isPresent {
+			stat[key] = true
+			walkRegion(bottom, c, hashes, stat)
+		}
+	}
+
+	if left := c - 1; left >= 0 && string(hashes[r][left]) == "1" {
+		i, j := strconv.Itoa(r), strconv.Itoa(left)
+		key := i + "-" + j
+
+		if _, isPresent := stat[key]; !isPresent {
+			stat[key] = true
+			walkRegion(r, left, hashes, stat)
+		}
+	}
 }
