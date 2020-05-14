c0bc66bb4ec30a1873444579ccdb1bf9260be4a1
Milo Lee <leexu.job@gmail.com>
2020-05-14 22:44:07 +0800

    2017: finish day 17 - part two

--- 2017/day-17-spinlock/main.go
+++ 2017/day-17-spinlock/main.go
@@ -6,16 +6,31 @@ import (
 
 func main() {
 	fmt.Println(partOne(324))
+	fmt.Println(partTwo(324))
 }
 
 func partOne(step int) int {
 	stack, curr := []int{0}, 0
 
 	for i := 1; i < 2018; i++ {
-		next := (curr+step)%len(stack) + 1
-		stack = append(stack[:next], append([]int{i}, stack[next:]...)...)
-		curr = next
+		curr = (curr+step)%len(stack) + 1
+		stack = append(stack[:curr], append([]int{i}, stack[curr:]...)...)
 	}
 
 	return stack[curr+1]
+}
+
+func partTwo(step int) int {
+	var value int
+	length, curr := 1, 0
+
+	for i := 1; i < 50000001; i++ {
+		curr = (curr+step)%length + 1
+		length = length + 1
+		if curr == 1 {
+			value = i
+		}
+	}
+
+	return value
 }
