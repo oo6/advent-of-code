f6b1e15143af6357129aa8b62670a5941d031b2e
Milo Lee <leexu.job@gmail.com>
2020-07-08 11:08:01 +0800

    2019: finish day 7 - part two

--- 2019/07-amplification-circui/main.ts
+++ 2019/07-amplification-circui/main.ts
@@ -1,74 +1,208 @@
 import readInputFile from "../read-input-file.ts";
 
-const input = readInputFile();
-
-enum OP {
-  ADD = 1,
-  MULT,
-  INPUT,
-  OUTPUT,
-  IF,
-  UNLESS,
-  LESS_THAN,
-  EQUAL,
-  HALT = 99,
+interface Context {
+  program: number[];
+  inputs: number[];
+  outputs: number[];
+  pos: number;
+  state: "cont" | "wait" | "halt";
 }
 
-const get = (list: number[], mode: number, value: number) => {
-  value = list[value];
-  return mode == 0 ? list[value] : value;
-};
+type Value = ["position" | "immediate", number];
 
-export const execute = (list: number[], inputs: number[]): number[] => {
-  list = [...list];
-  inputs = [...inputs];
-  let pos = 0;
+const get = (
+  list: number[],
+  mode: "position" | "immediate",
+  value: number,
+): number => (mode == "position" ? list[value] : value);
 
-  for (; pos < list.length;) {
-    const code = list[pos] % 100;
-    const mode = Math.floor(list[pos] / 100);
-    const first = get(list, mode % 10, pos + 1);
-    const second = get(list, Math.floor(mode / 10), pos + 2);
+const set = (list: number[], store: number, value: number): number[] => [
+  ...list.slice(0, store),
+  value,
+  ...list.slice(store + 1),
+];
 
-    switch (code) {
-      case OP.ADD:
-        list[list[pos + 3]] = first + second;
-        pos += 4;
-        break;
-      case OP.MULT:
-        list[list[pos + 3]] = first * second;
-        pos += 4;
-        break;
-      case OP.INPUT:
-        if (inputs.length) {
-          list[list[pos + 1]] = inputs.shift() as number;
-          pos += 2;
-        }
-        break;
-      case OP.OUTPUT:
-        inputs.push(first);
-        pos += 2;
-        break;
-      case OP.IF:
-        pos = first ? second : pos + 3;
-        break;
-      case OP.UNLESS:
-        pos = first ? pos + 3 : second;
-        break;
-      case OP.LESS_THAN:
-        list[list[pos + 3]] = first < second ? 1 : 0;
-        pos += 4;
-        break;
-      case OP.EQUAL:
-        list[list[pos + 3]] = first == second ? 1 : 0;
-        pos += 4;
-        break;
-      case OP.HALT:
-        return inputs;
-    }
+const add = (
+  { program, ...context }: Context,
+  left: Value,
+  right: Value,
+  [, store]: Value,
+): Context => ({
+  ...context,
+  program: set(
+    program,
+    store,
+    get(program, ...left) + get(program, ...right),
+  ),
+  state: "cont",
+});
+
+const mult = (
+  { program, ...context }: Context,
+  left: Value,
+  right: Value,
+  [, store]: Value,
+): Context => ({
+  ...context,
+  program: set(
+    program,
+    store,
+    get(program, ...left) * get(program, ...right),
+  ),
+  state: "cont",
+});
+
+const input = (context: Context, [, store]: Value): Context => {
+  if (context.inputs.length) {
+    return {
+      ...context,
+      inputs: context.inputs.slice(1),
+      program: set(context.program, store, context.inputs[0]),
+      state: "cont",
+    };
   }
 
-  throw "error";
+  return { ...context, state: "wait" };
+};
+
+const output = ({ outputs, ...context }: Context, value: Value): Context => ({
+  ...context,
+  outputs: [...outputs, get(context.program, ...value)],
+  state: "cont",
+});
+
+const jumpIf = (context: Context, condition: Value, where: Value): Context => {
+  if (get(context.program, ...condition)) {
+    return { ...context, pos: get(context.program, ...where), state: "cont" };
+  }
+
+  return { ...context, state: "cont" };
+};
+
+const jumpUnless = (
+  context: Context,
+  condition: Value,
+  where: Value,
+): Context => {
+  if (get(context.program, ...condition)) {
+    return { ...context, state: "cont" };
+  }
+
+  return { ...context, pos: get(context.program, ...where), state: "cont" };
+};
+
+const lessThan = (
+  { program, ...context }: Context,
+  left: Value,
+  right: Value,
+  [, store]: Value,
+): Context => ({
+  ...context,
+  program: set(
+    program,
+    store,
+    get(program, ...left) < get(program, ...right) ? 1 : 0,
+  ),
+  state: "cont",
+});
+
+const equal = (
+  { program, ...context }: Context,
+  left: Value,
+  right: Value,
+  [, store]: Value,
+): Context => ({
+  ...context,
+  program: set(
+    program,
+    store,
+    get(program, ...left) == get(program, ...right) ? 1 : 0,
+  ),
+  state: "cont",
+});
+
+const halt = (context: Context): Context => ({ ...context, state: "halt" });
+
+const Functions = {
+  1: add,
+  2: mult,
+  3: input,
+  4: output,
+  5: jumpIf,
+  6: jumpUnless,
+  7: lessThan,
+  8: equal,
+  99: halt,
+};
+
+const parameters = (
+  list: number[],
+  pos: number,
+  code: number,
+  length: number,
+): Value[] => {
+  let mode = Math.floor(code / 100);
+
+  return Array.from(Array(length), (_, i) => {
+    const value: Value = [mode % 10 ? "immediate" : "position", list[pos + i]];
+    mode = Math.floor(mode / 10);
+
+    return value;
+  });
+};
+
+const execute = (context: Context): Context => {
+  while (true) {
+    const code = context.program[context.pos];
+    const func = Functions[(code % 100) as 99] as (...args: any[]) => Context;
+    const newContext = func(
+      context,
+      ...parameters(context.program, context.pos + 1, code, func.length - 1),
+    );
+
+    if (newContext.state != "cont") {
+      return newContext;
+    }
+
+    const pos = context.pos == newContext.pos
+      ? context.pos + func.length
+      : newContext.pos;
+    context = { ...newContext, pos };
+  }
+};
+
+const executeOnce = (stack: Context[]): Context[] => {
+  return stack.reduce((acc, c, i) => {
+    acc.push(
+      execute({
+        ...c,
+        inputs: [
+          ...c.inputs,
+          ...(i ? acc[i - 1] : stack[stack.length - 1]).outputs,
+        ],
+        outputs: [],
+      }),
+    );
+    return acc;
+  }, [] as Context[]);
+};
+
+const executeLoop = (stack: Context[]): Context[] => {
+  if (stack.every((c) => c.state == "halt")) {
+    return stack;
+  }
+
+  return executeLoop(executeOnce(stack));
+};
+
+const initStack = (settings: number[]): Context[] => {
+  return settings.map((s, i) => ({
+    program: readInputFile().split(",").map((i) => ~~i),
+    inputs: i ? [s] : [s, 0],
+    outputs: [],
+    pos: 0,
+    state: "cont",
+  }));
 };
 
 const permutate = (list: number[]): number[][] => {
@@ -76,24 +210,27 @@ const permutate = (list: number[]): number[][] => {
     return [list];
   }
 
-  return list.reduce(
-    (acc, item, i) => {
-      const rest = permutate([...list.slice(0, i), ...list.slice(i + 1)]);
-      return acc.concat(rest.map((items) => [item, ...items]));
-    },
-    [] as number[][],
-  );
+  return list.reduce((acc, item, i) => {
+    const rest = permutate([...list.slice(0, i), ...list.slice(i + 1)]);
+    return acc.concat(rest.map((items) => [item, ...items]));
+  }, [] as number[][]);
 };
 
-const program = input.split(",").map((i) => ~~i);
-
 const partOne = (): number => {
-  const signals = permutate([0, 1, 2, 3, 4]).map((items) =>
-    items.reduce((acc, num) => {
-      return execute(program, [num, acc])[0];
-    }, 0)
+  const signals = permutate([0, 1, 2, 3, 4]).map(
+    (items) => executeOnce(initStack(items)).reverse()[0].outputs[0],
   );
+
+  return Math.max(...signals);
+};
+
+const partTwo = (): number => {
+  const signals = permutate([5, 6, 7, 8, 9]).map(
+    (items) => executeLoop(initStack(items)).reverse()[0].outputs[0],
+  );
+
   return Math.max(...signals);
 };
 
 console.log(partOne());
+console.log(partTwo());
