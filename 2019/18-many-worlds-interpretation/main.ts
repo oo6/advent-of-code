2ac203cada5c8ceb693c9e016e3dcd77ea47a7fc
Milo Lee <leexu.job@gmail.com>
2020-12-23 00:52:29 +0800

    temp

---
+++ 2019/18-many-worlds-interpretation/main.ts
@@ -0,0 +1,11 @@
+import readInputFile from "../read-input-file.ts";
+
+const input = readInputFile();
+
+const partOne = (): number => {
+  console.log(input);
+
+  return 0;
+};
+
+console.log(partOne());
