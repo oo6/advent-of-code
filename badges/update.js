b07e87c01b97277049fee907e57a7ff5bbafad4a
Milo Lee <leexu.dev@gmail.com>
2021-12-05 21:02:17 +0800

    📝 I just evaluated random code in my console

--- badges/update.js
+++ badges/update.js
@@ -11,9 +11,18 @@ function content(year, day, color) {
 
 fs.readdirSync(`${__dirname}/../`).forEach((year) => {
   if (2015 <= year && year <= new Date().getFullYear()) {
-    const day = Math.max(
-      ...fs.readdirSync(year).map((day) => ~~day.slice(0, 2))
-    );
+    let day;
+
+    if (year == 2021) {
+      day = Math.max(
+        ...fs
+          .readdirSync(`${year}/lib/`)
+          .filter((path) => fs.lstatSync(`${year}/lib/${path}`).isDirectory())
+          .map((day) => ~~day.slice(4, 6))
+      );
+    } else {
+      day = Math.max(...fs.readdirSync(year).map((day) => ~~day.slice(0, 2)));
+    }
     fs.writeFileSync(
       `${__dirname}/${year}.json`,
       content(year, day, day == 25 ? "brightgreen" : "orange")
