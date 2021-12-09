e9027666e871a44064eb94c1c8a8be4e342a2bcf
Milo Lee <leexu.dev@gmail.com>
2021-12-10 01:19:37 +0800

    2021: fix day 7 test error

--- 2021/lib/day_07/day_07.ex
+++ 2021/lib/day_07/day_07.ex
@@ -23,7 +23,10 @@ defmodule AOC2021.Day07 do
       |> String.split(",", trim: true)
       |> Enum.map(&String.to_integer/1)
 
-    average = floor(Enum.sum(positions) / length(positions))
-    Enum.reduce(positions, 0, &(&2 + Enum.sum(1..abs(&1 - average))))
+    average = Enum.sum(positions) / length(positions)
+
+    [floor(average), ceil(average)]
+    |> Enum.map(fn number -> Enum.reduce(positions, 0, &(&2 + Enum.sum(1..abs(&1 - number)))) end)
+    |> Enum.min()
   end
 end
